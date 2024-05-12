443c8da0a965f746e260d571f18ee2ba8f09e177
Nicolas2Primo <nicolasprimo@gmail.com>
2024-05-12 14:38:05 -0300

    fix: fix some codes

--- Work-2/Segmentation/Region-Growth/region_growth.py
+++ Work-2/Segmentation/Region-Growth/region_growth.py
@@ -5,7 +5,6 @@ def region_growing(image, seed, threshold):
     binary_image = np.zeros_like(image)
     pixels_to_check = [seed]
 
-    # Verifica se a semente está dentro dos limites da imagem
     seed_i, seed_j = seed
     if not (0 <= seed_i < height and 0 <= seed_j < width):
         raise ValueError("Seed coordinates are out of bounds")
@@ -25,7 +24,6 @@ def region_growing(image, seed, threshold):
                     neighbor_i = current_i + di
                     neighbor_j = current_j + dj
 
-                    # Tratamento de bordas aprimorado
                     if 0 <= neighbor_i < height and 0 <= neighbor_j < width:
                         pixels_to_check.append((neighbor_i, neighbor_j))
     
